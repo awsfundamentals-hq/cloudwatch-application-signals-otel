fe6c773a4962bad2a05bcc73d7a37d7ace122916
Tobias Schmidt <schmidt.tobias@protonmail.ch>
2025-01-19 11:49:04 +0100

    display infos from Fargate in frontend

--- sst.config.ts
+++ sst.config.ts
@@ -133,7 +133,7 @@ const createTaskDefinition = (params: { repositoryUrl: $util.Output<string>; tas
               },
             },
             healthCheck: {
-              command: ['CMD-SHELL', 'curl -f http://localhost:80/ || exit 1'],
+              command: ['CMD-SHELL', 'curl -f http://localhost/ || exit 1'],
               interval: 30,
               timeout: 5,
               retries: 3,
